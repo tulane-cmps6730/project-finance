2034906fec37545363006d6a2bbdeff0860b4357
Zachary Wiel <63222465+Eco-Zarch@users.noreply.github.com>
2024-04-14 12:19:45 -0500

    changes

--- nlp/classes.py
+++ nlp/classes.py
@@ -1,6 +1,5 @@
 from typing import Any, Text
 from openai import OpenAI
-from dotenv import load_dotenv
 from pathlib import Path
 from stability_sdk import api
 from stability_sdk.animation import AnimationArgs, Animator
@@ -8,8 +7,72 @@ from stability_sdk.utils import create_video_from_frames
 from tqdm import tqdm
 from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
 import math
-import os
+import json
 
+subject_type_prompt = {"market":"Based on the following reasearch articles, summarize if {} is doing well.",
+                       "ticker":"Based on the following reasearch articles, summarize if {} is doing well.",
+                       "news":"Based on the following reasearch articles, summarize the state of {}."}
+
+media_format_prompt = {"text": "Format this summary in the style of a morning brew newsletter", 
+                       "audio": "Format this summary as a script for a podcast. Make it very casual and conversational. Include phrases like umm and uhh to make it sound more natural. Add breaths where natural.", 
+                       "video": "Format this summary as a script for an informative video."}
+
+
+class ApiContext():
+    def __innit__(self, openai_api_key, stability_api_key, stability_host):
+        self.openai_api_key = openai_api_key
+        self.client = OpenAI(api_key=openai_api_key)
+        self.stability_context = api.Context(stability_host, stability_api_key)
+
+
+class Conversation():
+    def __init__(self, api_context: ApiContext):
+        self.request_list = []
+        self.client = api_context.client    
+    
+    def run_conversation(self):
+        while True:
+            try:
+                message = input("What would you like to learn about? I can teach you about the state of the market, a specific stock, or a current event. Please specify if you would like the information in text, audio, or video format.\n\n")
+                messages = [{"role": "user", "content": message}]
+                tools = [
+                    {
+                        "type": "function",
+                        "function": {
+                            "name": "media_output",
+                            "description": "Determine if the user wants to learn about the general state of the market, a specific stock, or a current event, and what format the user wants the information in. If a specific stock is requested, the stock ticker should be provided. If a current event is requested, the event name should be provided. The output should be in the requested format.",
+                            "parameters": {
+                                "type": "object",
+                                "properties": {
+                                    "media_format": {"type": "string", "enum": ["text", "audio", "video"]},
+                                    "subject_type": {"type": "string", "enum": ["market","ticker","news"]},
+                                    "subject": {"type": "string"}
+                                },
+                                #"required": ["subject", "media_format", "subject_type"],
+                            },
+                        },
+                    }
+                ]
+                response = self.client.chat.completions.create(
+                    model="gpt-3.5-turbo-0125",
+                    messages=messages,
+                    tools=tools,
+                    tool_choice="auto",
+                    )
+                
+                response_message = response.choices[0].message
+                tool_call = response_message.tool_calls[0]
+                arguments = json.loads(tool_call.function.arguments)
+                media_format = arguments["media_format"]
+                subject_type = arguments["subject_type"]
+                subject = arguments["subject"]
+                break
+            except Exception as e:
+                print(e)
+                print("Sorry, I didn't understand that. Please try again and clearly specify the subject and the desired media format output.")
+        
+        print(f"Here is the {media_format} information on the {subject_type} {subject}.")
+        return [media_format, subject_type, subject]
 
 
 class Request():
@@ -31,33 +94,19 @@ class WebScraper():
             return "news research"
 
 class Model():
-    def __init__(self, request: Request):
+    def __init__(self, request: Request, api_context: ApiContext):
         self.request = request
+        self.client = api_context.client
     
     def generate(self, research) -> Text:
-        load_dotenv()
-        api_key = os.getenv('OPENAI_API_KEY')
-        client = OpenAI(api_key=api_key)
 
-        if self.request.subject_type == "market":
-            message_subject = "Based on the following reasearch articles, summarize if" +self.request.subject+" is doing well."
-        elif self.request.subject_type == "ticker":
-            message_subject = "Based on the following reasearch articles, summarize if" +self.request.subject+" is doing well." 
-        elif self.request.subject_type == "news":
-            message_subject = "Based on the following reasearch articles, summarize the state of "+self.request.subject+"."
-
-
-        if self.request.media_format == "text":
-            message_format = "Format this summary in the style of a morning brew newsletter"
-        elif self.request.media_format == "audio":
-            message_format = "Format this summary as a script for a podcast. Make it very casual and conversational. Include phrases like umm and uhh to make it sound more natural. Add breaths where natural."
-        elif self.request.media_format == "video":
-            message_format = "Format this summary as a script for an informative video."
+        message_subject = subject_type_prompt[self.request.subject_type].format(self.request.subject)
+        message_format = media_format_prompt[self.request.media_format]
         
         
         message_thread = message_subject+message_format+"\n###\n" + research + "\n###"
         messages = [{"role": "system", "content": message_thread}]
-        response = client.chat.completions.create(
+        response = self.client.chat.completions.create(
                     model="gpt-3.5-turbo-0125",
                     messages=messages,
                     )
@@ -65,27 +114,17 @@ class Model():
         return content
 
 class Media():
-    def __init__(self, request: Request):
+    def __init__(self, request: Request, api_context: ApiContext):
         self.request = request
         self.base_dir = Path(__file__).parent  # Base directory for the script
         self.output_dir = self.base_dir / "output"  # Define a specific output directory
         self.output_dir.mkdir(exist_ok=True)  # Create the output directory if it doesn't exist
+        self.client = api_context.client
+        self.stability_context = api_context.stability_context
     
-    
-    
-    def api_context(self):
-        load_dotenv()
-        STABILITY_HOST = "grpc.stability.ai:443"
-        openai_api_key = os.getenv('OPENAI_API_KEY')
-        STABILITY_KEY = os.getenv('STABILITY_KEY')
-        
-        stability_context = api.Context(STABILITY_HOST, STABILITY_KEY)
-        openai_client = OpenAI(api_key=openai_api_key)
-        return stability_context, openai_client
-    
-    def generate_audio(self, content: Text, openai_client: OpenAI):
+    def generate_audio(self, content: Text):
         speech_file_path = self.output_dir / "speech.mp3"
-        response = openai_client.audio.speech.create(
+        response = self.client.audio.speech.create(
             model="tts-1",
             voice="onyx",
             input=content,
@@ -94,7 +133,7 @@ class Media():
         return speech_file_path
     
 
-    def generate_frames(self, content: Text, stability_context: api.Context):
+    def generate_frames(self, content: Text):
         frames_dir = self.output_dir / "video_frames"
         frames_dir.mkdir(exist_ok=True)  # Ensure the directory exists
         args = AnimationArgs()
@@ -103,7 +142,7 @@ class Media():
         }
         negative_prompt = ""
         animator = Animator(
-            api_context=stability_context,
+            api_context=self.stability_context,
             animation_prompts=animation_prompts,
             negative_prompt=negative_prompt,
             args=args,
@@ -135,10 +174,7 @@ class Media():
         return output_path
    
     
-    def generate_media(self, content: Text):
-        
-        stability_context, openai_client = self.api_context()
-        
+    def generate_media(self, content: Text):    
         if self.request.media_format == "text":
             text_path = self.output_dir / "news.txt"
             with open(text_path, "w") as f:
@@ -146,7 +182,7 @@ class Media():
             return text_path
         
         if self.request.media_format == "audio":
-            audio_path = self.generate_audio(content, openai_client)
+            audio_path = self.generate_audio(content)
             return audio_path
         
         if self.request.media_format == "video":
@@ -155,12 +191,10 @@ class Media():
             frames_dir = self.output_dir / "video_frames"
             create_video_from_frames(frames_dir, str(video_path), fps=24)
 
-            audio_path = self.generate_audio(content, openai_client)
+            audio_path = self.generate_audio(content)
 
             output_path = self.output_dir / "combined_video.mp4"
 
-            
-        
             return self.combine_audio_video(str(audio_path), str(video_path), str(output_path))
         
 
